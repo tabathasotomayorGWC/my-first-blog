aabe2b6d049b96541ebff2ff7dd34346a18a3cb4
syfgwc <tabathasotomayor@gmail.com>
2017-07-19 14:34:36 -0500

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '15gq@@g#7l*=5s=#746n1201v+%gm2%yysc^alv=4cpjg5#c#y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<syfgwcchi03>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'tsotomayorgwc.pythonanywhere.com']
 
 
 # Application definition
